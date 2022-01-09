23b4c2bbc5afb1f293a562d19dbdff149270fa7d
TomJia98 <Tomchristian1998@gmail.com>
2022-01-09 10:58:25 +0800

    cleaned up js and added comments

--- assets/script.js
+++ assets/script.js
@@ -3,34 +3,28 @@ const firstPage = document.querySelector("#first-page");
 const activeQuiz = document.querySelector("#questions-page");
 const highscoresPage = document.querySelector("#highscore-page");
 const highscoresFirstPage = document.querySelector("#highscores-firstpage");
-
 const highscoreDisplay = document.querySelector("#highscore-list");
 const timer = document.querySelector("#timer");
 const questionEl =  document.querySelector("#question");
-const buttonsEl =  document.querySelector("#buttons");
 const guessEl = document.querySelector("#is-guess");
 const enterInitals = document.querySelector("#enter-initials");
 const submitButton = document.querySelector("#submit");
 const initials = document.querySelector("#initials");
-
 const resetScores = document.querySelector("#reset");
-
+const buttonsEl =  document.querySelector("#buttons");
 const buttonAnswer1 = document.getElementById("answer 1");
 const buttonAnswer2 = document.getElementById("answer 2");
 const buttonAnswer3 = document.getElementById("answer 3");
 const buttonAnswer4 = document.getElementById("answer 4");
 
-// var scoresArray = [];
-
-// if (localStorage.getItem("scores")==undefined){
-// localStorage.setItem("scores", scoresArray);
-// }
-
 //getting all the associated divs from the HTML
 
 var timeLeft = 60;
 var highscoreTime = 0;
+var currentQuestion = 0;
+var gameNotTimedOut = false;
 
+//setting up all the working variables to be changed later
 
 const question1 = {
     question: "In javascript, what character should follow each finished line of code?",
@@ -92,8 +86,8 @@ const question5 = {
     answer4: "==",
     isAnswer4: false
     }
-  
-    var gameNotTimedOut = false;
+  //adding all the questions, all following the same semantics
+    
 
 function countdownTimer() {
   var timeInterval = setInterval(function () {
@@ -101,24 +95,14 @@ function countdownTimer() {
     timer.textContent = timeLeft;
     timeLeft--;
 
-    if (timeLeft < 1 && !gameNotTimedOut) {
+    if (timeLeft < 0 && !gameNotTimedOut) {
+      timeLeft = "DNF"
       clearInterval(timeInterval);
-
-      timer.textContent = "Game Over";
-      
       loadHighscore();
     }
-  
-      
-
-
-    
-
   }, 1000);
-
-
-
 };
+//setting up the timer, with interupts to change it when a guess is wrong, and to stop it if the quiz is over
 
 function loadQ2(){
   questionEl.textContent = question2.question;
@@ -183,21 +167,17 @@ buttonAnswer3.setAttribute("data-isAnswer", question5.isAnswer3);
 buttonAnswer4.textContent = question5.answer4;
 buttonAnswer4.setAttribute("data-isAnswer", question5.isAnswer4);
 };
+//creating all the load functions to change which question and answer pairs are being displayed
 function loadHighscore(){
   activeQuiz.setAttribute("style", "display:none;");
   enterInitals.setAttribute("style", "display:inline;");
   highscoreTime = timeLeft;
 gameNotTimedOut = true;
-
-
-
 }
+//loads the initials input section, stores the time at quiz finish, and stops the timer
 
-
-const loadQuestions = [
-  loadQ2, loadQ3, loadQ4, loadQ5, loadHighscore
-];
-var currentQuestion = 0;
+const loadQuestions = [loadQ2, loadQ3, loadQ4, loadQ5, loadHighscore];
+//sets the order of questions, and loads highscore page after last question
 
 
 function guess(isCorrect) {
@@ -207,8 +187,6 @@ function guess(isCorrect) {
       guessEl.textContent = "";
   }, 500);
   }
-
- 
   else if (isCorrect == "no"){
     guessEl.textContent = "incorrect";
     timeLeft = timeLeft - 10;    
@@ -217,7 +195,7 @@ function guess(isCorrect) {
   }, 500);
   }
 }
-
+//adds the incorrecct/correct popup upon making a guess and clears it 500ms later, removes 10 seconds from a wrong guess
 
 function showHighscores(){
   enterInitals.setAttribute("style", "display:none;");
@@ -225,98 +203,80 @@ function showHighscores(){
   highscoresPage.setAttribute("style", "display:inline;")
 
   var hScores = JSON.parse(localStorage.getItem("scores"));
-  console.log(hScores.length);
   for (var i = 0; i < hScores.length; i++){
-var liEl = document.createElement("li");
-liEl.textContent = hScores[i];
-highscoreDisplay.appendChild(liEl);
+      var liEl = document.createElement("li");
+      liEl.textContent = hScores[i];
+      highscoreDisplay.appendChild(liEl);
   }
-  // highscoreDisplay.textContent = JSON.parse(localStorage.getItem("scores"));
-  // highscoreDisplay.textContent = localStorage.getItem("scores");
-
-
 }
+//sets the highscore section to active, checks and displays the current highscores in localStorage
+
 highscoresFirstPage.addEventListener("click", showHighscores)
+//allows the highscore button on pagestart to link to highscores
 
 
 beginQuizButton.addEventListener("click", function startQuiz(){
-firstPage.setAttribute("style", "display:none;");
-activeQuiz.setAttribute("style", "display:inline;");
-countdownTimer();
-// sets the question section of the quiz as active 
-questionEl.textContent = question1.question;
+      firstPage.setAttribute("style", "display:none;");
+      activeQuiz.setAttribute("style", "display:inline;");
+      countdownTimer();
+      // sets the question section of the quiz as active and starts the timer
+      questionEl.textContent = question1.question;
 
-buttonAnswer1.textContent = question1.answer1;
-buttonAnswer1.setAttribute("data-isAnswer", question1.isAnswer1);
+      buttonAnswer1.textContent = question1.answer1;
+      buttonAnswer1.setAttribute("data-isAnswer", question1.isAnswer1);
 
-buttonAnswer2.textContent = question1.answer2;
-buttonAnswer2.setAttribute("data-isAnswer", question1.isAnswer2);
+      buttonAnswer2.textContent = question1.answer2;
+      buttonAnswer2.setAttribute("data-isAnswer", question1.isAnswer2);
 
-buttonAnswer3.textContent = question1.answer3;
-buttonAnswer3.setAttribute("data-isAnswer", question1.isAnswer3);
+      buttonAnswer3.textContent = question1.answer3;
+      buttonAnswer3.setAttribute("data-isAnswer", question1.isAnswer3);
 
-buttonAnswer4.textContent = question1.answer4;
-buttonAnswer4.setAttribute("data-isAnswer", question1.isAnswer4);
-
-
+      buttonAnswer4.textContent = question1.answer4;
+      buttonAnswer4.setAttribute("data-isAnswer", question1.isAnswer4);
 })
+//loads the first set of questions
 
 buttonsEl.addEventListener('click', function isAnswer(event){
   if (event.target.dataset.isanswer == "true"){
 
-guess("yes");
-
-loadQuestions[currentQuestion]();
-currentQuestion++;
+      guess("yes");
+      loadQuestions[currentQuestion]();
+      currentQuestion++;
   }
 else  if (event.target.dataset.isanswer == "false"){
-  guess("no");
 
-  loadQuestions[currentQuestion]();
-  currentQuestion++;
-}
-
-})
+      guess("no");
+      loadQuestions[currentQuestion]();
+      currentQuestion++;
+}})
+// checks if the selected answer is correct, subtracts 10 seconds from timer if not, loads next question for both
 
 submitButton.addEventListener("click", function addHighscore(event){
 event.preventDefault();
 var currentHighscore = initials.value;
-// enterInitals.setAttribute("style", "display:none;");
-// highscoresPage.setAttribute("style", "display:inline;")
-
-// var newLi = document.createElement('li');
-
-
-var newLi = currentHighscore + " - " + highscoreTime + "s";
+var newLi = currentHighscore + " - " + highscoreTime;
 var x = []
 if (localStorage.getItem("scores") != undefined ){
-  console.log("its defined");
 var a1 = localStorage.getItem("scores");
 var a2 = JSON.parse(a1);
 console.log(a2);
 for (var i = 0; i < a2.length; i++){
   x.push(a2[i]);
-}
-}
-
-
-
+}}
+//checks if localStorage contains anything, and adds it to an array if it does
 x.push(newLi);
-
 var z =JSON.stringify(x) 
-
 localStorage.setItem("scores", z);
-
 showHighscores();
 })
+//adds the new score to an array(which might contain previous scores), and sends it to localStorage, loads highscores
 
 resetScores.addEventListener("click", function reset(){
   highscoreDisplay.setAttribute("style", "display:none;")
 localStorage.clear();
 showHighscores();
-
 })
-
+//clears the highscore page and resets the localStorage upon clicking the reset button 
 
 
 
